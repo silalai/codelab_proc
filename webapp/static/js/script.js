865b85128882c7e14dacf467ce5a39cb53c102fe
silalai <109204699+silalai@users.noreply.github.com>
2022-10-03 16:50:46 +0700

    Update script.js

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -1,4 +1,22 @@
-$(document).ready(function () {
+$(document).ready(function () {  
+  $('#btnSendMsg').click( function(){
+    // url from where we want to get the data
+    var socket = io('http://localhost:8000/join-now');
+     console.log("SendMsg");
+
+     data_input = { "msg": $('#txtMessage').val(), "name": $('#txtName').val()};
+
+     socket.emit('joidRoom', data_input, function (data) {
+            console.log('sent message to server!');
+            console.log(data);
+            //
+            Swal.fire({
+                title: 'ขอบคุณที่บอกฉัน',
+                text: data,
+            })
+    });
+
+});
   var socket = io('http://localhost:3000');
   socket.on('hi', function(msg) {
     Swal.fire({
@@ -14,6 +32,7 @@ $(document).ready(function () {
       } 
     })
   });
+
 });
 
 function AppendData(val){
