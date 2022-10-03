c3f27763e3efcefea26a507f8305fe7f39c12e79
silalai <109204699+silalai@users.noreply.github.com>
2022-10-03 16:49:26 +0700

    Update urls.py

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -3,11 +3,11 @@
 from django.urls import path
 from .views import (
     list_view, 
-   
+    client
 )
 
 app_name = 'main'
 urlpatterns = [
     path('', list_view, name='home-list'),
-
+    path('join-now', client, name='hangout'),
 ]
