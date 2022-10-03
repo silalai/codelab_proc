e68cb5f9f72c3de56dba76cf0bdf59a2efcb2321
silalai <109204699+silalai@users.noreply.github.com>
2022-10-03 16:49:51 +0700

    Update views.py

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -6,3 +6,10 @@ def list_view(request):
     actors = [{"id": '1',"name": 'Rocket falconnine'}, {"id": '2',"name": 'Dragon lowrence'}]
     
     return render(request, "main/home.html",  {'actors': actors})
+
+def client(request):
+    factors = [{"id": '1',"name": 'ข้อความที่จะฝากไว้ให้เธอ'}, {"id": '2',"name": '[บอกชื่อเทอกับฉัน]'}]
+    
+    return render(request, "main/55.html",  {'factors': factors})
+
+
